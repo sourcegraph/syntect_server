ae70261ab5327a498282f26d42c6c0d0897b6560
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-08-28 12:12:36 -0700

    also push docker image to us.gcr.io/sourcegraph-dev/syntect_server

--- publish.sh
+++ publish.sh
@@ -1,1 +1,5 @@
 docker push sourcegraph/syntect_server
+
+# Deploy to us.gcr.io/sourcegraph-dev as well (we use our own registry).
+docker tag sourcegraph/syntect_server us.gcr.io/sourcegraph-dev/syntect_server
+gcloud docker -- push us.gcr.io/sourcegraph-dev/syntect_server
