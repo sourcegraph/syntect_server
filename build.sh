f320d3ae24f1a2ef4d08b4f3615c2d7e7ee92391
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-08-28 22:59:18 -0700

    rm .cargo/config (can be specified via `cargo rustc -- -C 'linker=x86_64-linux-musl-gcc'`

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-env 'CC_x86_64-unknown-linux-musl=x86_64-linux-musl-gcc' cargo build --release --target x86_64-unknown-linux-musl
+env 'CC_x86_64-unknown-linux-musl=x86_64-linux-musl-gcc' cargo rustc --release --target x86_64-unknown-linux-musl -- -C 'linker=x86_64-linux-musl-gcc'
 docker build -t sourcegraph/syntect_server .
