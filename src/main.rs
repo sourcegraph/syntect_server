b3da7ab34a425e71b6ba01015641e3ef868bcf6c
Matt King <kingy895@gmail.com>
2018-02-21 11:54:15 -0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+#[macro_use]
+extern crate lazy_static;
+extern crate rayon;
 extern crate rocket;
-extern crate serde_json;
 #[macro_use]
 extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 extern crate syntect;
-#[macro_use]
-extern crate lazy_static;
-extern crate rayon;
 
 use rocket_contrib::{Json, Value};
 use syntect::parsing::SyntaxSet;
@@ -36,34 +36,33 @@ struct Query {
 #[post("/", format = "application/json", data = "<q>")]
 fn index(q: Json<Query>) -> Json<Value> {
     SYNTAX_SET.with(|syntax_set| {
-	    // Determine theme to use.
-	    //
-	    // TODO(slimsag): We could let the query specify the theme file's actual
-	    // bytes? e.g. via `load_from_reader`.
-	    let theme = match THEME_SET.themes.get(&q.theme) {
-	        Some(v) => v,
-	        None => return Json(json!({"error": "invalid theme"})),
-	    };
-
-        // Fall back: Determine syntax definition by first line.
-        let syntax_by_first_line_def = match syntax_set.find_syntax_by_first_line(&q.code) {
+        // Determine theme to use.
+        //
+        // TODO(slimsag): We could let the query specify the theme file's actual
+        // bytes? e.g. via `load_from_reader`.
+        let theme = match THEME_SET.themes.get(&q.theme) {
             Some(v) => v,
-            None => return Json(json!({"error": "invalid syntax"})),
+            None => return Json(json!({"error": "invalid theme"})),
         };
 
         // Determine syntax definition by extension.
         let syntax_def = match syntax_set.find_syntax_by_extension(&q.extension) {
             Some(v) => v,
-            None => syntax_by_first_line_def,
+            None =>
+                // Fall back: Determine syntax definition by first line.
+                match syntax_set.find_syntax_by_first_line(&q.code) {
+                    Some(v) => v,
+                    None => return Json(json!({"error": "invalid syntax"})),
+            },
         };
 
-	    // TODO(slimsag): return the theme's background color (and other info??) to caller?
-	    // https://github.com/trishume/syntect/blob/c8b47758a3872d478c7fc740782cd468b2c0a96b/examples/synhtml.rs#L24
+        // TODO(slimsag): return the theme's background color (and other info??) to caller?
+        // https://github.com/trishume/syntect/blob/c8b47758a3872d478c7fc740782cd468b2c0a96b/examples/synhtml.rs#L24
 
-	    Json(json!({
+        Json(json!({
 	        "data": highlighted_snippet_for_string(&q.code, &syntax_def, theme),
 	    }))
-	})
+    })
 }
 
 #[get("/health")]
@@ -87,19 +86,21 @@ fn list_features() {
 
     // List supported file extensions.
     SYNTAX_SET.with(|syntax_set| {
-                        println!("## Supported file extensions:");
-                        println!("");
-                        for sd in syntax_set.syntaxes() {
-                            println!("- {} (`{}`)", sd.name, sd.file_extensions.join("`, `"));
-                        }
-                        println!("");
-                    });
+        println!("## Supported file extensions:");
+        println!("");
+        for sd in syntax_set.syntaxes() {
+            println!("- {} (`{}`)", sd.name, sd.file_extensions.join("`, `"));
+        }
+        println!("");
+    });
 }
 
 fn main() {
     // Only list features if QUIET != "true"
     match env::var("QUIET") {
-        Ok(v) => if v != "true" { list_features() },
+        Ok(v) => if v != "true" {
+            list_features()
+        },
         Err(_) => list_features(),
     };
 
