3c1c2acbeef29a3de70ff5df5f0d1fcebbe6079f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-06-05 02:00:58 -0700

    prevent caching when adding binary to Docker images

--- build.debian.sh
+++ build.debian.sh
@@ -1,3 +1,3 @@
 set -ex
 env 'CC_x86_64-unknown-linux-musl=musl-gcc' cargo rustc --release --target x86_64-unknown-linux-musl -- -C 'linker=musl-gcc'
-docker build -t sourcegraph/syntect_server .
+docker build --no-cache -t sourcegraph/syntect_server .
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -ex
 env 'CC_x86_64-unknown-linux-musl=x86_64-linux-musl-gcc' cargo rustc --release --target x86_64-unknown-linux-musl -- -C 'linker=x86_64-linux-musl-gcc'
-docker build -t sourcegraph/syntect_server .
+docker build --no-cache -t sourcegraph/syntect_server .
